caaa3321070e461e19d7dd6eca5844bdc7cc4871
Neetu Kumari <neetusuthar90@gmail.com>
2023-07-17 10:47:53 +0530

    add load_user function

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_login import LoginManager
+from flask_login import LoginManager, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -32,3 +32,7 @@ def set_password(self,password):
 
 def check_password(self,password):
     return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
